3cc85b1c1d07957b5bf0391055ae67c2f00921c3
Martin Sauve <martin.sauve@gmail.com>
2016-05-30 18:13:49 -0400

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/listContacts', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
