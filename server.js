84c8a7adf566ba9c741dde3a8835c66d9c2a8722
Martin Sauve <martin.sauve@gmail.com>
2016-05-30 14:07:40 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+
+app.get('/listContacts', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+app.get('/contact/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       users = JSON.parse( data );
+       var user = users["contact" + req.params.id]
+       console.log( user );
+       res.end( JSON.stringify(user));
+   });
+})
+
+app.get('/addUser/:id/:name/:phone/:profession', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var newUser = JSON.parse("{ \"name\" : \""+req.params.name+"\",\"phone\":\""+req.params.phone+"\",\"profession\":\""+req.params.profession+"\",\"id\":\""+req.params.id+"\"}");
+
+       data["contact"+req.params.id] = newUser;
+       console.log( data );
+       fs.writeFile( __dirname + "/" + "users.json", JSON.stringify(data));
+       res.end( "contact added");
+   });
+
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
